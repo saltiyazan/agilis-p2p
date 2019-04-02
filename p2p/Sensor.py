8a604007d83258e63182ea6e6e5c6005baf241c0
Nánási Dániel <nanasi.daniel@wigner.mta.hu>
2019-04-02 22:14:33 +0200

    Sensor osztály, változói és függvényeinek implementálása a protokollhoz.

---
+++ p2p/Sensor.py
@@ -0,0 +1,49 @@
+class Sensor:
+    def __init__(self, id, servers):
+        self.id = id
+        self.servers = servers
+        self.new_datas=[]
+        self.has_new_data=False
+        self.recieved_messages=[]
+        self.message_to_send=""
+
+    #szerverek tömbjének újramegadása
+    def redefine_servers(self, servers_array):
+        self.servers=servers_array
+    
+    #kikeresi a küldendő adatot a meglévő még nem küldött adatok kozül
+    def search_data_to_send(self, data):
+        for d in self.new_datas:
+            if (data == d):
+                data_to_send=data
+                break
+        else:
+            raise Exception(data + " data has not yet by the sensor.")
+        return data_to_send
+    
+    #megpróbál küldeni a szervernek
+    def try_to_send_data(self, server, data):
+        return True
+
+    #sorban megpróbál küldeni az összes szervernek
+    def send_data(self, data):
+        for server in self.servers:
+            success = self.try_to_send_data(server, data)
+            if (success):
+                break
+        else:
+            raise Exception("Could not send data to any server.")
+    
+    def recieve_msg(self, message):
+        self.recieved_messages.__add__(message)
+
+    #üzenetküldés próbája az összes szerver felé
+    def send_message(self, message):
+        for server in self.servers:
+            success = self.try_to_send_data(server, message)
+            if (success):
+                break
+        else:
+            raise Exception("Could not send message to any server.")
+    
+    
