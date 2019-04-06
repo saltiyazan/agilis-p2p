562684acd121c303adbb18c00d36274f0a66b927
engedi.cs <engedi.cs@gmail.com>
2019-04-06 23:10:30 +0200

    Travis + dockerhez szükséges két Dockerfile, deploy script és travis zml hozzáadása

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#create networks and containers
+for i in $(seq 1 $1);
+do
+		#network
+        docker network create \
+          --driver=bridge \
+          --subnet=10.0.$i.0/24 \
+          --gateway=10.0.$i.1 \
+          br$i
+
+		#server
+        docker create --name h$i \
+          --network br$i \
+		  --expose 9600 \
+          --publish 10.0.$i.1:9600:9600 \
+          engedics/p2p-server
+        docker start h$i
+
+		#sensors
+        for o in $(seq 1 $2);
+        do
+				port=`echo "9600 + $o" | bc`
+                docker create --name s$i-$o \
+                  --network br$i \
+				  --expose $port \
+				  --publish 10.0.$i.1:$port:$port \
+                  engedics/p2p-sensor
+                docker start s$i-$o
+        done
+done
