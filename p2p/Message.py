79976b2a67e006387fdd93b8345b597a4785e917
Nánási Dániel <nanasi.daniel@wigner.mta.hu>
2019-04-08 16:28:19 +0200

    Message class v2

--- p2p/Message.py
+++ p2p/Message.py
@@ -7,3 +7,13 @@ class Message:
         self.parent_server_id=server_id
         self.is_parent_alive=is_alive
         self.content=content
+
+#Készült egy általánosabb megoldása is a message üzenetnek, szerdán megvitatjuk, melyik a szükségesebb.
+class MessageV2:
+    def __init__(self, source_id, destination_id, content, source_is_server, destination_is_server):
+        self.source_id = source_id
+        self.destination_id = destination_id
+        self.content = content
+        self.is_sent = False
+        self.source_is_server = source_is_server # bool
+        self.destination_is_server = destination_is_server #bool
