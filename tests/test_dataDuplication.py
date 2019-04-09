d86c4295f540d14d22a596751788dda770383b52
KT <KT@PC>
2019-04-10 01:04:30 +0200

    test added: dataDuplication, dataReceivedAfterRecovery, serverDown

---
+++ tests/test_dataDuplication.py
@@ -0,0 +1,22 @@
+h="""
+Egy szerveren nem szabad ketszer ugyanazt az adatot tarolni.
+Egy adatot ketszer kuldunk el a szervernek.
+Elvart viselkedes: csak egyszer jelenjen meg a szerveren
+"""
+
+from p2p.Sensor import Sensor
+from p2p.StorageServer import StorageServer
+
+print(h)
+
+server = StorageServer(alive=True, id=1)
+s_id = 10
+sensor = Sensor(id=s_id, servers=[server])
+data_ = "test_data"
+
+sensor.send_data(data=data_)
+sensor.send_data(data=data_)
+
+assert(server.data[s_id].count(data_) == 1)
+
+
---
+++ tests/test_dataReceivedAfterRecovery.py
@@ -0,0 +1,33 @@
+h="""
+Egy szerver ujraeledese utan ra kell tolteni a kihagyott adatokat
+Egy szenzornak megadunk ket szervert.
+A main szerver leallasa utan a szenzor a backup szervernek kuldi az adatokat.
+A main szerver feleledese utan meg kell kapnia a kimaradt adatokat.
+"""
+
+from p2p.Sensor import Sensor
+from p2p.StorageServer import StorageServer
+
+print(h)
+
+mainServer = StorageServer(alive=True, id=1)
+backupServer = StorageServer(alive=True, id=2)
+
+s_id = 10
+sensor = Sensor(id=s_id, servers=[mainServer, backupServer])
+
+data_1 = "data_1 to mainServer"
+sensor.send_data(data=data_1)
+assert(data_1 in mainServer.data[s_id])
+assert(backupServer.data == {})
+
+#mainServer leallitasa
+mainServer.alive = False
+data_2 = "data_2 during main down"
+sensor.send_data(data=data_2)
+assert(data_2 not in mainServer.data[s_id])
+assert(data_2 in backupServer.dead_servers_data[1][s_id])
+
+#main feleled
+mainServer.alive = True
+assert(data_2 in mainServer.data[s_id])
