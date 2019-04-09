de16d54199ba4b5ab75c10b0ca66eb98b486c2cb
KT <KT@PC>
2019-04-10 01:13:48 +0200

    fix: adding neighbours to servers

--- tests/test_dataReceivedAfterRecovery.py
+++ tests/test_dataReceivedAfterRecovery.py
@@ -12,6 +12,11 @@ print(h)
 
 mainServer = StorageServer(alive=True, id=1)
 backupServer = StorageServer(alive=True, id=2)
+mainServer.add_neighbour_server(backupServer)
+backupServer.add_neighbour_server(mainServer)
+
+print("main szomszedok: ", mainServer.neighbour_servers)
+print("backup szomszedok: ", backupServer.neighbour_servers)
 
 s_id = 10
 sensor = Sensor(id=s_id, servers=[mainServer, backupServer])
