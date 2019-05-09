1bfa0ace846cb8c9e1003c3759748a74b2c6d2c8
engedi.cs <engedi.cs@gmail.com>
2019-05-09 23:46:36 +0200

    RPYC

--- p2p/StorageServer.py
+++ p2p/StorageServer.py
@@ -52,8 +52,8 @@ class StorageServerService(rpyc.Service):
 
     #még csak egyszerűen hozzáadjuk a szomszédok listájához
     def exposed_refresh_neighbour_list(self, new_neighbour_list):
-        new_neighbour_list.remove(self.default_gateway)
         self.neighbour_servers = new_neighbour_list
+        self.neighbour_servers.remove(self.default_gateway)
         self.log('Refreshed neighbour server list:', self.neighbour_servers)
         for sensor_id in self.sensors:
             self.send_neighbour_list(sensor_id)
